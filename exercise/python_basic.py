4d15444782532835fbf95f4c95e9e66c5f2b9e8f
zhangsx <zhangsxnaive@163.com>
2018-10-08 16:38:35 +0800

    init project

---
+++ exercise/python_basic.py
@@ -0,0 +1,14 @@
+# 格式化代码
+print('%2d-%02d' % (3, 1))
+print('%.2f' % 3.1415926)
+
+# list
+classmates = ['zhangsx', "zhangjq", "niuxs", "fujl", 'zhoujh', 'zhangyf', 'wusy', 'cuix']
+num = len(classmates)
+classmates.pop()
+
+# tuple
+classmates = ('zhangsx','zhangjq','niuxs','fujl')
+print(classmates)
+
+# 条件判断
